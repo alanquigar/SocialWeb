addf69cbc87efeda1be81a4d1095538e5078fff6
Alan Quigar <sombi2010@hotmail.com>
2022-10-19 22:09:20 -0600

    4to

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const persons = [
         lastname: "Quinones",
         phone: "656-572-3869",
         age: 21,
+        genero: "Masculino",
         city: "Juarez",
         id: "1"
     },
@@ -16,6 +17,7 @@ const persons = [
         lastname: "Venegas",
         phone: "614-524-8562",
         age: 19,
+        genero: "Masculino",
         city: "Chihuahua",
         id: "2"
     },
@@ -24,17 +26,23 @@ const persons = [
         lastname: "Franco",
         phone: "656-124-5252",
         age: 23,
+        genero: "Masculino",
         city: "Juarez",
         id: "3"
     }
 ]
 
 const typeDefs = gql`
+    enum Genero {
+        Masculino
+        Femenino
+    }
     type Person {
         name: String!
         lastname: String!
         phone: String!
         age: Int!
+        genero: String!
         city: String!
         id: ID!
     }
@@ -42,7 +50,15 @@ const typeDefs = gql`
     type Query {
         personCount: Int!
         allPersons: [Person]!
-        findPerson(name: String!): Person
+        findPerson(
+            name: String
+            lastname: String
+            phone: String
+            age: Int
+            genero: String
+            city: String
+            id: ID
+            ): Person
     }
 
     type Mutation {
@@ -51,7 +67,13 @@ const typeDefs = gql`
             lastname: String!
             phone: String!
             age: Int!
+            genero: String!
             city: String!
+            id: ID!
+        ): Person
+       editNumber(
+        name: String!
+        phone: String!
         ): Person
     }
 `
@@ -73,7 +95,19 @@ const resolvers = {
             const person = {...args,id:uuid()}
             persons.push(person)
             return person
+        },
+        editNumber: (root,args) => {
+            const personIndex = persons.findIndex(p => p.name == args.name)
+            if(personIndex == -1) return null
+
+            const person = persons[personIndex]
+            
+            const updatedPerson  = {...person, phone: args.phone}
+
+            persons[personIndex] = updatedPerson
+            return updatedPerson
         }
+        
     }
 }
 
